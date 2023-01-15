feb591bae1ed129c7f71d284ed6621a5230aa081
reagan <reaganteflon@gmail.com>
2023-01-15 04:35:54 +0300

    fixed url bug

--- employee/urls.py
+++ employee/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from employee import views  
 
 urlpatterns = [  
-    path('admin/', admin.site.urls),  
-    path('emp', views.emp),  
+    path('admin/', admin.site.urls),
+    path('', views.emp), 
+    path('emp', views.emp), 
     path('show',views.show),  
     path('edit/<int:id>', views.edit),  
     path('update/<int:id>', views.update),  
